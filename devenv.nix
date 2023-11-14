481a3be321d672f60cfc53fab51495a498bccb9f
Marcus Ramberg <marcus@means.no>
2023-11-14 15:25:11 +0100

    ci: Attempt to run tests directly instead of via pre-commit

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,6 @@
 
   pre-commit.hooks.gofmt.enable = true;
   pre-commit.hooks.govet.enable = true;
-  pre-commit.hooks.gotest.enable = true;
+  # pre-commit.hooks.gotest.enable = true;
   pre-commit.hooks.commitizen.enable = true;
 }
