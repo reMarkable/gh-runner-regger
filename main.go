03f1b5d28f756b38ba143c34e0345bff6500ea93
Marcus Ramberg <marcus@means.no>
2023-11-14 08:50:58 +0100

    feat: initial import

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/helmfile/vals"
+	runnerreg "github.com/remarkable/gh-runner-regger/pkg/github"
+)
+
+func getJWT(pemBytes []byte, appID string) (string, error) {
+	signingKey, err := jwt.ParseRSAPrivateKeyFromPEM(pemBytes)
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, jwt.MapClaims{
+		"iat": time.Now().Unix(),
+		"exp": time.Now().Add(time.Minute * 10).Unix(),
+		"iss": appID,
+	})
+	tokenString, err := token.SignedString(signingKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func getSecretVal(pemPath string, signBytes *[]byte) error {
+	// get the secret value
+	runtime, err := vals.New(vals.Options{
+		LogOutput:             os.Stdout,
+		CacheSize:             100,
+		ExcludeSecret:         true,
+		FailOnMissingKeyInMap: true,
+	})
+	if err != nil {
+		return err
+	}
+
+	signString, err := runtime.Get(pemPath)
+	if err != nil {
+		return err
+	}
+	byteSlice := []byte(signString)
+	signBytes = &byteSlice
+	return nil
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Printf("Usage: %s <path/to/private-key.pem|ref+$vals-expression> <app-id> <org>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	pemPath := os.Args[1]
+	appID := os.Args[2]
+	org := os.Args[3]
+
+	// if pemPath starts with the string "ref+", we should use vals to fetch the value
+	var signBytes []byte
+	var err error
+	if strings.HasPrefix(pemPath, "ref+") {
+		getSecretVal(pemPath, &signBytes)
+	} else {
+		signBytes, err = os.ReadFile(pemPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+
+	tokenString, err := getJWT(signBytes, appID)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	headers := http.Header{
+		"Authorization": []string{fmt.Sprintf("Bearer %s", tokenString)},
+		"Accept":        []string{"application/vnd.github.v3+json"},
+	}
+
+	installationID, err := runnerreg.GetInstallationID(headers)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	installationToken, err := runnerreg.GetInstallationToken(installationID, headers)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	runnerToken, err := runnerreg.GetRunnerToken(installationToken, org)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(runnerToken)
+}
---
+++ pkg/github/runnerreg.go
@@ -0,0 +1,93 @@
+package runnerreg
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Structs for parsing JSON responses
+type Installation struct {
+	ID int `json:"id"`
+}
+
+type Token struct {
+	Token string `json:"token"`
+}
+
+func GetInstallationID(headers http.Header) (int, error) {
+	url := "https://api.github.com/app/installations"
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, err
+	}
+
+	req.Header = headers
+	resp, err := client.Do(req)
+	if err != nil {
+		return 0, err
+	}
+	defer resp.Body.Close()
+
+	var installations []Installation
+	err = json.NewDecoder(resp.Body).Decode(&installations)
+	if err != nil {
+		return 0, err
+	}
+
+	return installations[0].ID, nil
+}
+
+func GetInstallationToken(installationID int, headers http.Header) (string, error) {
+	url := fmt.Sprintf("https://api.github.com/app/installations/%d/access_tokens", installationID)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header = headers
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var token Token
+	err = json.NewDecoder(resp.Body).Decode(&token)
+	if err != nil {
+		return "", err
+	}
+
+	return token.Token, nil
+}
+
+func GetRunnerToken(installationToken, orgName string) (string, error) {
+	url := fmt.Sprintf("https://api.github.com/orgs/%s/actions/runners/registration-token", orgName)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	headers := http.Header{
+		"Authorization": []string{fmt.Sprintf("token %s", installationToken)},
+		"Accept":        []string{"application/vnd.github.v3+json"},
+	}
+	req.Header = headers
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	var token Token
+	err = json.NewDecoder(resp.Body).Decode(&token)
+	if err != nil {
+		return "", err
+	}
+
+	return token.Token, nil
+}
