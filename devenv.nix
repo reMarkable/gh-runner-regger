c99b3caa196d15e338050b5d0f3978c5853e4270
Marcus Ramberg <marcus@means.no>
2023-11-14 13:32:16 +0100

    feat: github action CI

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,8 @@
 
   languages.go.enable = true;
 
-  pre-commit.hooks.golangci-lint.enable = true;
+  pre-commit.hooks.gofmt.enable = true;
+  pre-commit.hooks.govet.enable = true;
+  pre-commit.hooks.gotest.enable = true;
   pre-commit.hooks.commitizen.enable = true;
 }
