b7684a36e05cb89bc44e81db36d4c44ec795264e
Marcus Ramberg <marcus.ramberg@remarkable.no>
2024-12-12 08:05:22 +0100

    chore: add more linting

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,7 @@
 
   pre-commit.hooks.gofmt.enable = true;
   pre-commit.hooks.govet.enable = true;
+  pre-commit.hooks.golangci-lint.enable = true;
   pre-commit.hooks.gotest.enable = true;
   pre-commit.hooks.commitizen.enable = true;
 }
