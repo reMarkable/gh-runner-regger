507e118d616042230c556bb3aac0f13afbcee7ea
Marcus Ramberg <marcus.ramberg@remarkable.no>
2025-02-05 14:38:13 +0100

    ci: port gomod flake from helmfile-nix

--- devenv.nix
+++ devenv.nix
@@ -1,13 +1,28 @@
-{ pkgs, ... }:
-
 {
-  packages = [ pkgs.git ];
-
-  languages.go.enable = true;
-
-  pre-commit.hooks.gofmt.enable = true;
-  pre-commit.hooks.govet.enable = true;
-  pre-commit.hooks.golangci-lint.enable = true;
-  pre-commit.hooks.gotest.enable = true;
-  pre-commit.hooks.commitizen.enable = true;
+  pkgs,
+  inputs,
+  system,
+  ...
+}:
+let
+  goEnv = inputs.gomod2nix.legacyPackages.${system}.mkGoEnv { pwd = ./.; };
+in
+{
+  packages = with pkgs; [
+    goEnv
+    inputs.gomod2nix.legacyPackages.${system}.gomod2nix
+    docker
+    helmfile
+    kubernetes-helm
+  ];
+  pre-commit.hooks = {
+    gofmt.enable = true;
+    govet.enable = true;
+    golangci-lint.enable = true;
+    gotest.enable = true;
+    commitizen.enable = true;
+  };
+  enterTest = ''
+    nix build .
+  '';
 }
---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "gh-runner-regger";
+
+  inputs.devenv.url = "github:cachix/devenv";
+  inputs.devenv.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.gomod2nix.url = "github:nix-community/gomod2nix";
+  inputs.gomod2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.gomod2nix.inputs.flake-utils.follows = "flake-utils";
+
+  outputs =
+    {
+      devenv,
+      flake-utils,
+      gomod2nix,
+      nixpkgs,
+      self,
+    }@inputs:
+    (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        # The current default sdk for macOS fails to compile go projects, so we use a newer one for now.
+        # This has no effect on other platforms.
+        callPackage = pkgs.darwin.apple_sdk_11_0.callPackage or pkgs.callPackage;
+      in
+      {
+        packages.default = callPackage ./. {
+          inherit (gomod2nix.legacyPackages.${system}) buildGoApplication;
+        };
+        packages.devenv-up = self.devShells.${system}.default.config.procfileScript;
+        packages.devenv-test = self.devShells.${system}.default.config.test;
+        devShells.default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            (import ./devenv.nix {
+              inherit inputs pkgs system;
+            })
+          ];
+        };
+      }
+    ));
+}
