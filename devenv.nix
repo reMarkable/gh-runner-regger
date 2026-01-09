489c385bcb7168fb7f305506f48dabd5c6739b42
Marcus Ramberg <marcus.ramberg@remarkable.no>
2026-01-09 11:58:04 +0100

    fix: add prek config

--- devenv.nix
+++ devenv.nix
@@ -5,11 +5,4 @@
 
   languages.go.enable = true;
 
-  git-hooks.hooks = {
-    gofmt.enable = true;
-    govet.enable = true;
-    golangci-lint.enable = true;
-    gotest.enable = true;
-    commitizen.enable = true;
-  };
 }
